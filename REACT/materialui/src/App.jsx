6423171115918349c5348fcbebea4b839fcfabb5
Antonio Rosales Martínez <antoniorosalesmartinez90@gmail.com>
2026-03-10 21:14:05 +0100

    feat material

---
+++ REACT/materialui/src/App.jsx
@@ -0,0 +1,13 @@
+import ModalCustom from "./components/ModalCustom";
+import MasonryImageList from "./components/Gallery";
+
+const App = () => {
+  return (
+    <>
+      <ModalCustom />
+      <MasonryImageList />
+    </>
+  );
+};
+
+export default App;
